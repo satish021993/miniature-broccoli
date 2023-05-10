60a3ad71e367688846ee36097c8592226f9d1fc4
satish021993 <122429324+satish021993@users.noreply.github.com>
2023-05-10 23:20:10 +0530

    Add files via upload

---
+++ highest.py
@@ -0,0 +1,28 @@
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+for i in chosen_word:
+  display.append("_")
+
+guess = input("Guess a letter: ").lower()
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for letter in chosen_word:
+    if letter == guess:
+      i = chosen_word.index(guess)
+      display[i] = guess
+      
+
+print(display)
